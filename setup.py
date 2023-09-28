6259a0b000ee96918355b48f62c309892cc11cee
Deepak2200 <119037072+Deepak2200@users.noreply.github.com>
2023-09-28 22:41:45 +0530

    upadting all file

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools 
+
+__version__ ="0.0.1"
+
+
+with open("README.md","r") as f:
+    long_description=f.read()
+
+
+REPO_NAME = "fsdsCNNClassifier"
+AUTHOR_NAME = "DEEPAK YADAV"
+SRC_REPO = "CNNClassifier"
+AUTHOR_Email= "ydeepakdy123@gamil.com"
+
+
+setuptools.setup(
+    name = SRC_REPO,
+    version=__version__,
+    author = AUTHOR_NAME,
+    author_email=AUTHOR_Email,
+    description= "this is my classification project",
+    long_description=long_description,
+    long_description_content = "text/markdown",
+    url = f"https://github.com/{AUTHOR_NAME}/issues",
+
+    project_urls = {
+        "Bug Tracker": f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}/issues",
+
+
+
+    },
+
+    package_dir={"":"src"},
+    packages = setuptools.find_packages(where="src")
+)
